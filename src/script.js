aae12c7b2ebe2b6557c8dfb41e1a2246d950bcfb
subans198 <subans198@gmail.com>
2026-01-24 20:04:26 +0530

    Update script.js

--- src/script.js
+++ src/script.js
@@ -1,29 +1,15 @@
-console.log("Chennai Aqua Water Purifier website loaded");
-
 const slider = document.getElementById("slider");
 
-/* IMAGE + PRICE DATA */
 const images = [
-  { src: "images/1.jpg", price: "₹ 7,000" },
-  { src: "images/2.jpg", price: "₹ 8,000" },
-  { src: "images/3.jpg", price: "₹ 9,500" }
+  "images/1.jpg",
+  "images/2.jpg",
+  "images/3.jpg"
 ];
 
-/* LOAD IMAGES WITH PRICE */
-images.forEach(item => {
-  const wrapper = document.createElement("div");
-  wrapper.className = "slide";
-
+images.forEach(src => {
   const img = document.createElement("img");
-  img.src = item.src;
-
-  const price = document.createElement("div");
-  price.className = "price";
-  price.textContent = item.price;
-
-  wrapper.appendChild(img);
-  wrapper.appendChild(price);
-  slider.appendChild(wrapper);
+  img.src = src;
+  slider.appendChild(img);
 });
 
 let index = 0;
